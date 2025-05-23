9d48028889e1a44152d62624f1b47b801aa28b77
Abenezer365 <abenezerkibebe@gmail.com>
2025-05-23 21:43:46 +0300

    Changed repo name

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/netflix/',
+  base: '/netflix-clone/',
   plugins: [react()],
 })
